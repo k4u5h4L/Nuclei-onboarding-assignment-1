793c27973c601be8f997368294c66fe358a1d2d1
k4u5h4L <kaushal.v.bhat@gmail.com>
2022-01-06 12:47:03 +0530

    refactor code to be modular

--- src/main/java/Assignment1.java
+++ src/main/java/Assignment1.java
@@ -3,27 +3,46 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
+class InvalidArguementsException extends Exception {
+
+  InvalidArguementsException(String s) {
+    super(s);
+  }
+}
+
+class StopLoopException extends Exception {
+
+  StopLoopException(String s) {
+    super(s);
+  }
+}
+
 public class Assignment1 {
 
-  public static void main(String[] args) {
+  String itemName, itemType;
+  int itemPrice, itemQuantity;
+
+  /*
+   * Function to validate if the command line arguements passed are in the correct form
+   * */
+  private static void validateInputs(String[] params) throws InvalidArguementsException {
     // check if -name and -type are present by checking the number of arguements
     // and names
-    if (args.length < 4 || !Arrays.asList(args).contains("-name") || !Arrays.asList(args)
+    if (params.length < 4 || !Arrays.asList(params).contains("-name") || !Arrays.asList(params)
         .contains("-type")) {
-      System.out.println("-name and -type not present. Exiting...");
-      System.exit(1);
-    } else if (!args[0].equals("-name")) {
-      System.out.println("-name should be present first. Exiting...");
-      System.exit(1);
+//      System.out.println("-name and -type not present. Exiting...");
+      throw new InvalidArguementsException("-name and -type not present.");
+    } else if (!params[0].equals("-name")) {
+//      System.out.println("-name should be present first. Exiting...");
+      throw new InvalidArguementsException("-name should be present first.");
     }
+  }
 
-    // arraylist to store all the items
-    ArrayList<Item> items = new ArrayList<Item>();
-
-    // scanner to scan the user input from the cli
-    Scanner scanner = new Scanner(System.in);
-
-    // a hashmap to store all the command line arguements
+  /*
+   * Function to parse all the command line arguements and return a hashmap
+   */
+  private static HashMap<String, String> getArguements(String[] args)
+      throws InvalidArguementsException {
     HashMap<String, String> item = new HashMap<String, String>();
 
     for (int i = 0; i < args.length - 1; i = i + 2) {
@@ -34,22 +53,92 @@ public class Assignment1 {
     // check if type is present as it is mandatory
     if (!item.containsKey("-type")) {
       // send not there message and quit
-      System.out.println("-type argument not provided. Exiting...");
-      System.exit(1);
+      throw new InvalidArguementsException("-name should be present first.");
     }
     // if price is not present, assign a default value
     if (!item.containsKey("-price")) {
+      System.out.println("-price wasn't provided, using 0 as default value...");
       item.put("-price", "0");
     }
     // if quantity is not present, assign a default value
     if (!item.containsKey("-quantity")) {
+      System.out.println("-quantity wasn't provided, using 0 as default value...");
       item.put("-quantity", "0");
     }
 
+    return item;
+  }
+
+  private static void validateTypeInput(HashMap<String, String> item)
+      throws InvalidArguementsException {
     if (!(item.get("-type").equals("raw") || item.get("-type").equals("manufactured") || item.get(
         "-type").equals("imported"))) {
-      System.out.println(
-          "-type should have only 3 types of values: raw, manufactured and " + "imported");
+      throw new InvalidArguementsException(
+          "-type should have only 3 types of values: raw, manufactured and imported");
+    }
+  }
+
+  private static void printer(ArrayList<Item> items) {
+    System.out.println("\n\nYour items are:\n");
+
+    // print each item and its attributes
+    for (Item it : items) {
+      System.out.println("Item name: " + it.getName());
+      System.out.println("Item price: " + it.getPrice());
+      System.out.println("Item quantity: " + it.getQuantity());
+      System.out.println("Item type: " + it.getType());
+      System.out.println("Item sales tax: " + it.getSalesTax());
+      System.out.println("Item final price: " + it.getFinalPrice() + "\n\n");
+    }
+  }
+
+  private static Item scanner(Scanner scan) throws StopLoopException {
+    System.out.print("Do you want to enter details of any other item (y/n): ");
+    char choice = scan.next().charAt(0);
+
+    if (choice == 'n' || choice == 'N') {
+      throw new StopLoopException("User interrupted.");
+    }
+
+    System.out.print("Enter item name: ");
+    String itemName = scan.next();
+    System.out.print("Enter item price: ");
+    int itemPrice = scan.nextInt();
+    System.out.print("Enter item quantity: ");
+    int itemQuantity = scan.nextInt();
+    System.out.print("Enter item type (raw, manufactured, imported): ");
+    String itemType = scan.next();
+
+    return new Item(itemName, itemPrice, itemQuantity, itemType);
+  }
+
+  public static void main(String[] args) {
+    try {
+      validateInputs(args);
+    } catch (InvalidArguementsException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    // arraylist to store all the items
+    ArrayList<Item> items = new ArrayList<Item>();
+
+    // scanner to scan the user input from the cli
+    Scanner scan = new Scanner(System.in);
+
+    // a hashmap to store all the command line arguements
+    HashMap<String, String> item = new HashMap<String, String>();
+    try {
+      item = getArguements(args);
+    } catch (InvalidArguementsException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    try {
+      validateTypeInput(item);
+    } catch (InvalidArguementsException e) {
+      e.printStackTrace();
       System.exit(1);
     }
 
@@ -59,117 +148,17 @@ public class Assignment1 {
 
     // keep iterating until user says no
     while (true) {
-      System.out.print("Do you want to enter details of any other item (y/n): ");
-      char choice = scanner.next().charAt(0);
-
-      if (choice == 'n' || choice == 'N') {
+      try {
+        items.add(scanner(scan));
+      } catch (StopLoopException e) {
         break;
       }
-
-      System.out.print("Enter item name: ");
-      String itemName = scanner.next();
-      System.out.print("Enter item price: ");
-      int itemPrice = scanner.nextInt();
-      System.out.print("Enter item quantity: ");
-      int itemQuantity = scanner.nextInt();
-      System.out.print("Enter item type (raw, manufactured, imported): ");
-      String itemType = scanner.next();
-
-      items.add(new Item(itemName, itemPrice, itemQuantity, itemType));
     }
 
-    System.out.println("Your items are:\n");
+    printer(items);
 
-    for (Item it : items) {
-      System.out.println("Item name: " + it.getName());
-      System.out.println("Item price: " + it.getPrice());
-      System.out.println("Item quantity: " + it.getQuantity());
-      System.out.println("Item type: " + it.getType());
-      System.out.println("Item sales tax: " + it.getSalesTax());
-      System.out.println("Item final price: " + it.getFinalPrice());
-    }
-
-    scanner.close();
+    scan.close();
   }
 }
 
-class Item {
 
-  private String name;
-  private int price;
-  private int quantity;
-  private String type;
-  private double salesTax;
-  private double finalPrice;
-
-  Item(String name, int price, int quantity, String type) {
-    this.name = name;
-    this.price = price;
-    this.quantity = quantity;
-    this.type = type;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getPrice() {
-    return this.price;
-  }
-
-  public void setPrice(int price) {
-    this.price = price;
-  }
-
-  public int getQuantity() {
-    return this.quantity;
-  }
-
-  public void setQuantity(int quantity) {
-    this.quantity = quantity;
-  }
-
-  public String getType() {
-    return this.type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public double getSalesTax() {
-    calculateSalesTax();
-    return this.salesTax;
-  }
-
-  public double getFinalPrice() {
-    calculateSalesTax();
-    return this.finalPrice;
-  }
-
-  private void calculateSalesTax() {
-    if (type.equals("raw")) {
-      salesTax = 0.125 * price;
-    } else if (type.equals("manufactured")) {
-      salesTax = (0.125 * price) + (0.02 * (price + (0.125 * price)));
-    } else {
-      double surcharge;
-      double importDuty = (0.1 * price) + price;
-      if (importDuty < 100) {
-        surcharge = 5;
-      } else if (importDuty < 200) {
-        surcharge = 10;
-      } else {
-        importDuty = 0.1 * price;
-        importDuty = price + importDuty;
-        surcharge = 0.05 * importDuty;
-      }
-      finalPrice = importDuty + surcharge;
-      salesTax = finalPrice - price;
-    }
-  }
-}
